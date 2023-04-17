323f3cf9853c740b5018f2e9c761b76f965d013f
Oooutman <mingren654@gmail.com>
2023-04-18 00:03:08 +0800

    Site updated: 2023-04-18 00:03:07

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,48 @@
 // 暗中观察
+// 等待文档加载完成
 $(document).ready(function() {
+    // 获取背景图像元素
     var backgroundImage = jQuery('#background-image');
-    // 如果背景图片元素不存在，则返回
+    // 如果背景图像元素不存在，则返回
     if (backgroundImage.length === 0) {
       return;
     }
+    // 获取board元素
     var board = jQuery('#board');
+    // 如果board元素不存在，则返回
     if (board.length === 0) {
       return;
     }
-    // 定义两个标志变量，分别表示背景图片是否应该根据窗口大小和页面滚动高度显示
+    // 定义posDisplay和scrollDisplay变量，用于控制背景图像的显示
     var posDisplay = false;
     var scrollDisplay = false;
-    // 定义一个函数，用于设置背景图片的位置
+    // 定义setBackgroundImagePos函数，用于设置背景图像的位置
     var setBackgroundImagePos = function() {
-      // 计算文章区块左侧与浏览器窗口左侧的距离
+      // 获取board元素的左边距
       var boardLeft = board[0].getClientRects()[0].left;
-      // 如果距离大于等于50，则更新posDisplay变量的值
+      // 根据左边距是否大于等于50来决定是否显示背景图像
       posDisplay = boardLeft >= 50;
-      // 根据posDisplay和scrollDisplay变量的值来设置背景图片的bottom属性
+      // 设置背景图像的display和left属性
       backgroundImage.css({
         'display': posDisplay && scrollDisplay ? 'block' : 'none',
         'left' : boardLeft - 136 + 'px'
       });
     };
-    // 调用setBackgroundImagePos函数设置初始位置
+    // 调用setBackgroundImagePos函数
     setBackgroundImagePos();
-    // 监听窗口大小改变事件，在窗口大小改变时重新调用setBackgroundImagePos函数
+    // 在窗口大小调整时重新调用setBackgroundImagePos函数
     jQuery(window).resize(setBackgroundImagePos);
-    // 监听页面滚动事件
+    // 获取board元素的顶部偏移量
     var headerHeight = board.offset().top;
+    // 监听页面滚动事件
     Fluid.utils.listenScroll(function() {
-      // 计算页面滚动高度
+      // 获取滚动高度
       var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
-      // 如果页面滚动高度大于等于文章区块顶部的位置，则更新scrollDisplay变量的值
+      // 根据滚动高度是否大于等于headerHeight来决定是否显示背景图像
       scrollDisplay = scrollHeight >= headerHeight;
-      // 根据posDisplay和scrollDisplay变量的值来设置背景图片隐藏显示
+      // 设置背景图像的display属性
       backgroundImage.css({
         'display': posDisplay && scrollDisplay ? 'block' : 'none'
       });
     });
-  });
+});
